f6e7077e9d42fe69a0c07da596bce0f2b0dea1ca
Srikanth Aravamuthan <aravamu2@users.noreply.github.com>
2023-01-16 13:10:04 -0600

    add tick marks around column names for formula
    - app.R: regression analysis tab
      - mod: add `str_c("`", ., "`")`
    
    #31 error in odds ratio and forest plot panels for pivot table tab
    #32 error in summary, ANOVA, and coefficient plot panels for regression analysis tab
    #33 add tick marks around column names for formula

--- app/app.R
+++ app/app.R
@@ -988,8 +988,8 @@ server <- function(input, output) {
           `CALVING_EASE_>=2` = ifelse(EASE >= 2, 1, 0),
           `CALVING_EASE_>=3` = ifelse(EASE >= 3, 1, 0),
           CNUM = ifelse((CNUM != 0) | (EASE == 0), CNUM, NA),
-          CSEX = ifelse(!is.na(CNUM),CSEX, NA),
-          CLIVE = ifelse(!is.na(CNUM),CLIVE, NA),
+          CSEX = ifelse(!is.na(CNUM), CSEX, NA),
+          CLIVE = ifelse(!is.na(CNUM), CLIVE, NA),
           TWINS = 
             case_when(
               CNUM > 1 ~ 1,
@@ -1264,6 +1264,7 @@ server <- function(input, output) {
     )
     
     input$explanatory %>% 
+      str_c("`", ., "`") %>% 
       str_flatten(collapse = " + ") %>% 
       str_c("`", input$response, "`", " ~ ", .) %>% 
       as.formula()
